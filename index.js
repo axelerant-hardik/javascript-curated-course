f46ea60fac4d3d2d4498848df9ac4d45e83f0b31
axelerant-hardik <hardik.pandya@axelerant.com>
2024-06-28 17:55:46 +0530

    Exercise: Max of two numbers

---
+++ index.js
@@ -0,0 +1,10 @@
+// Pass two numbers and get the max of the two.
+function getMaxOfTwoNumbers(a, b) {
+    return a > b ? a : b;
+}
+
+console.log(getMaxOfTwoNumbers(10, 5));
+console.log(getMaxOfTwoNumbers(3, 7));
+console.log(getMaxOfTwoNumbers(0, 2));
+console.log(getMaxOfTwoNumbers(-1, 1));
+console.log(getMaxOfTwoNumbers(-10, -11));
